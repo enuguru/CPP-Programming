74790457acd4359217525ab128095a9043209232
GUrurajan <machinelearningprofessional@gmail.com>
2020-10-05 11:31:50 +0530

    all files

--- data_types_one/enum_struct_class/2.enum.cpp
+++ data_types_one/enum_struct_class/2.enum.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-enum year{Jan, Feb, Mar, Apr, May, Jun, Jul, 
+enum year{Jan = 1, Feb, Mar, Apr, May, Jun, Jul, 
 		Aug, Sep, Oct, Nov, Dec}; 
 
 int main() 
